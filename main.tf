2855dc89004b26a81d322b866936454d52f9e74b
hsumyatthwe <hsumyatthwe@gtmh-telecom.com>
2022-03-07 13:31:45 +0630

    commit instial

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "my_ubuntu" {
-    ami = "ami-0892d3c7ee96c0bf7"
-    instance_type = "t3.micro"
+  ami           = "ami-0892d3c7ee96c0bf7"
+  instance_type = "t3.micro"
 
-    tags = {
-        Name = "My-Ununtu-Server"
-        Owner = "Hsu Myat Thwe"
-    }
+  tags = {
+    Name  = "My-Ununtu-Server"
+    Owner = "Hsu Myat Thwe"
+  }
 }
