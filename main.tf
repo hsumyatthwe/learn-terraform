e1b6c8796064dfbdccb1d0ae9bf7a6321d9f1b50
hsumyatthwe <hsumyatthwe@gtmh-telecom.com>
2022-03-07 13:30:01 +0630

    commit for creating ubuntu aws instatnce

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "my_ubuntu" {
+    ami = "ami-0892d3c7ee96c0bf7"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "My-Ununtu-Server"
+        Owner = "Hsu Myat Thwe"
+    }
+}
