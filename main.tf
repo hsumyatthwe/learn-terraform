b98b8dac166cf83434154a7c79edb72ad5b7fd0b
hsumyatthwe <hsumyatthwe@gtmh-telecom.com>
2022-03-07 14:11:28 +0630

    commit keyname

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-0892d3c7ee96c0bf7"
   instance_type = "t3.micro"
+  key_name = "key-hmt-oregon"
 
   tags = {
     Name  = "My-Ununtu-Server"
